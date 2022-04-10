ab10f85a1d0e9086d36d2dd8cd483a4363746161
“Robson <pitelsound@gmail.comGit config --global user.email pitelsound@gmail.com>
2022-04-10 20:42:15 -0300

    apreedendo teste automatizado

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,8 +1,17 @@
 ///<reference types="cypress" />
 
 context ('Funcionalidade Login', () =>{
-    it ('Deve fazer Login com sucesso' , () => {
+
+    beforeEach(() => {
         cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    afterEach ( () => {
+        cy.screenshot ()
+
+    } );     
+    
+    it ('Deve fazer Login com sucesso' , () => {
+
         cy.get('#username').type ('aluno_ebac@teste.com')
         cy.get('#password').type ('teste@teste.com')
         cy.get('.woocommerce-form > .button').click ()
@@ -10,14 +19,14 @@ context ('Funcionalidade Login', () =>{
 
     })
     it ('Deve exibir uma mensagem de erro ao inserir usuario ' , () => {
-        cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+  
         cy.get('#username').type ('ebac@teste.com')
         cy.get('#password').type ('teste@teste.com')
         cy.get('.woocommerce-form > .button').click ()
         cy.get('.woocommerce-error > li') .should ('contain', 'desconhecido')
             })
      it ('Deve exibir uma mensagem de erro ao inserir senha errada invalida ' , () => {
-                cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
+    
                 cy.get('#username').type ('aluno_ebac@teste.com')
                 cy.get('#password').type ('teste@teste')
                 cy.get('.woocommerce-form > .button').click ()
